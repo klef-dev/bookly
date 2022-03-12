7a92afb8c5750e19544c4860344b44241c926187
klefcodes <abrahamugbeshe@gmail.com>
2022-03-12 21:29:21 +0100

    perf: added necessary types

--- server/src/controllers/BookController.ts
+++ server/src/controllers/BookController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 import { googleBookApi } from '../helpers';
+import { defaultErrorDto, responseDto } from '../types';
 
 export class BookController {
   public search = async (req: Request, res: Response) => {
@@ -18,15 +19,35 @@ export class BookController {
     }: { query: string; limit: number; offset: number } = req.query as any;
 
     try {
-      const data = await googleBookApi.get(
+      let {
+        data: { items, totalItems },
+        status,
+      }: responseDto = await googleBookApi.get(
         `/volumes?q=${query}
         &maxResults=${limit}&startIndex=${offset}&printType=books&orderBy=relevance&key=${
           process.env.GOOGLE_BOOK_API_KEY || ''
         }`
       );
-      return res.status(data.status).json(data.data);
+
+      items = items.map((item) => {
+        const { volumeInfo } = item;
+        return {
+          volumeInfo: {
+            title: volumeInfo.title,
+            authors: volumeInfo.authors,
+            description: volumeInfo.description,
+            imageLinks: { thumbnail: volumeInfo.imageLinks.thumbnail },
+            categories: volumeInfo.categories,
+            averageRating: volumeInfo?.averageRating,
+            ratingsCount: volumeInfo?.ratingsCount,
+          },
+        };
+      });
+
+      return res.status(status).json({ items, totalItems });
     } catch (error) {
-      return res.status(400).json({ error });
+      const { message, status } = error as defaultErrorDto;
+      return res.status(status).json({ message });
     }
   };
 }
---
+++ server/src/types/index.ts
@@ -0,0 +1,26 @@
+export interface defaultErrorDto {
+  status: number;
+  message: string;
+}
+
+export interface responseDto {
+  status: number;
+  data: googleAPIResponseDto;
+}
+
+interface googleAPIResponseDto {
+  items: {
+    volumeInfo: {
+      title: string;
+      authors: string[];
+      description: string;
+      imageLinks: {
+        thumbnail: string;
+      };
+      categories: string[];
+      averageRating?: number;
+      ratingsCount?: number;
+    };
+  }[];
+  totalItems: number;
+}
